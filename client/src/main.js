f884fc6e7dad4869c77cde466fac6f860224eeb2
liang0124 <2609233927@qq.com>
2017-11-20 19:05:49 +0800

    config Goodslist

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import store from './store'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
-Vue.use(VueAxios, Axios)
+import VueLazyload from 'vue-lazyload'
+import VueInfiniteScroll from 'vue-infinite-scroll'
+import {currency} from './util/currency'
 
-Vue.use(ElementUI)
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/product.css'
+
+Vue.use(VueInfiniteScroll)
+Vue.use(VueLazyload, {
+  loading: 'static/loading-svg/loading-bars.svg',
+  try: 3
+})
+Vue.use(VueAxios, Axios)
+Vue.filter('currency', currency)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -21,9 +32,31 @@ new Vue({
   template: '<App/>',
   components: { App },
   mounted () {
-    this.$http.get('/goods/list?page=1&pageSize=3&sort=1&priceLevel=all')
-    .then(res => {
-      console.log(res.data.result)
-    })
+    this.checkLogin()
+    this.getCartCount()
+  },
+  methods: {
+    checkLogin () {
+      this.$http.get('/user/checkLogin')
+      .then(res => {
+        res = res.data
+        if (res.status === 0) {
+          this.$store.commit('updateUserInfo', res.result)
+        } else {
+          if (this.$route.path !== '/') {
+            this.$router.push('/')
+          }
+        }
+      })
+    },
+    getCartCount () {
+      this.$http.get('/user/getCartCount')
+      .then(res => {
+        res = res.data
+        if (res.status === 0) {
+          this.$store.commit('updateCartCount', res.result)
+        }
+      })
+    }
   }
 })
