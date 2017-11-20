95d0af09da91fce5c4157e90a0ffb79ca96f15c7
liang0124 <2609233927@qq.com>
2017-11-20 13:06:48 +0800

    项目初始化

---
+++ client/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <el-switch active-color="red" inactive-color="green" v-model="value1">
+    </el-switch>
+    <div class="block">
+      <span class="demonstration">默认不区分颜色</span>
+      <el-rate v-model="value2"></el-rate>
+    </div>
+    <div class="block">
+      <span class="demonstration">区分颜色</span>
+      <el-rate
+        v-model="value3"
+        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+      </el-rate>
+    </div>
+    <el-rate
+      v-model="value4"
+      :icon-classes="['icon-rate-face-1', 'icon-rate-face-2', 'icon-rate-face-3']"
+      void-icon-class="icon-rate-face-off"
+      :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+    </el-rate>
+    <el-rate
+      v-model="value5"
+      disabled
+      show-score
+      text-color="#ff9900"
+      score-template="{value}">
+    </el-rate>
+    <router-view/>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      value1: true,
+      value2: 2,
+      value3: 3,
+      value4: null,
+      value5: 4
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App'
+import router from './router'
+// import store from './store'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, Axios)
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  // store,
+  template: '<App/>',
+  components: { App },
+  mounted () {
+    this.$http.get('/goods/list?page=1&pageSize=3&sort=1&priceLevel=all')
+    .then(res => {
+      console.log(res.data.result)
+    })
+  }
+})
